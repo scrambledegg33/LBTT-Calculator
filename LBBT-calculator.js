a7ef41b27a45542de75952623ac184ee9098c1f9
Henry Grainger <henry.grainger@gmail.com>
2022-11-20 20:21:31 +0000

    code is functional

--- LBBT-calculator.js
+++ LBBT-calculator.js
@@ -1,10 +1,8 @@
-let standBuyer = [0, 0.02, 0.05, 0.10, 0.12];
-
+let standBuyerRates = [0, 0.02, 0.05, 0.10, 0.12];
 let taxBand = [0, 145000, 250000, 325000, 750000, Infinity];
 
-let totalTax = 0;
 
-let calTaxSum = function (houseCost, i) {
+function calTaxSum(houseCost, i) {
 let loBand = taxBand[i];
 let upBand = taxBand[i+1];
 if ((houseCost - loBand) <= 0) {
@@ -16,56 +14,29 @@ return (upBand - loBand);
 }
 }
 
-let calTax = function (taxableSum, percentage) {
-return taxableSum * percentage;
+function calTax(taxableSum, percentage) {
+    return taxableSum * percentage;
 }
 
-let storeTax = function(tax) {
-totalTax += tax;
-}
 
-let getTotalTax = function() {
-return totalTax;
-}
-
-let calEffRate = function(totalTax, houseCost) {
-
-return (totalTax / houseCost) * 100;
-}
-
-let stampCalc = function(houseCost) {
-if (houseCost >= 0) {
-for (let i = 0; i < buyerType.length; i++) {
-let taxableSum = calTaxSum(houseCost, i);
-if (taxableSum === 0) {
-console.log("there is no tax to be paid")
-
-} else {
-
-//8.Calculate Tax
-let tax = calTax(taxableSum, buyerType[i]);
-
-console.log("You owe " + tax + " in tax.");
-}
-}
-
-//Total Tax
-let totalTax = updateTotalTax();
-
-updateEffectiveRate(totalTax, houseCost);
-}
+function stampCalc(houseCost) {
+    let totalTax = 0;
+    if (houseCost >= 0) {
+        for (let i = 0; i < standBuyerRates.length; i++) {
+            let taxableSum = calTaxSum(houseCost, i);
+            if (taxableSum === 0) {
+                return "there is no tax to be paid."
+            } 
+            else {
+                let tax = calTax(taxableSum, standBuyerRates[i]);
+                if (tax > 0){
+                    totalTax += tax;
+                }
+            }
+        }
+    }
+    return totalTax;
 };
 
-let updateTotalTax = function() {
-//11. Get total Tax
 
-let totalTax = getTotalTax();
-displayTotalTax(totalTax);
-return totalTax;
-}
-
-let updateEffectiveRate = function(totalTax, houseCost) {
-//12. Calcualate Effective Rate
-let effRate = calEffRate(totalTax, houseCost);
-
-}
+console.log(stampCalc(900000));
---
+++ test/LBBT-calculator.test.js
@@ -0,0 +1,9 @@
+const { default: test } = require('node:test')
+const { describe } = require('yargs')
+const LbbtCalculator = require('../LBBT-calculator.js')
+
+describe('tests for the LBBT calculator', () => {
+    test('', () => {
+        
+    })
+})
